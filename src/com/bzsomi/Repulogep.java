2d420a28b41fe1327fa386577fe72a9a3d5fa316
Zsombor <brozsek.zsombor@petrik.hu>
2020-10-25 20:25:59 +0100

    Repulogep osztály

---
+++ src/com/bzsomi/Repulogep.java
@@ -0,0 +1,11 @@
+package com.bzsomi;
+
+public class Repulogep {
+    private String azonosito;
+    private int tankMeret;
+
+    public Repulogep(String azonosito, int tankMeret) {
+        this.azonosito = azonosito;
+        this.tankMeret = tankMeret;
+    }
+}
